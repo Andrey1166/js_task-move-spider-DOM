65dd27503815488850c527b1f0eae0333a24d8ce
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-08 04:34:02 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,37 +3,22 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-document.addEventListener('click', (e) => {
+wall.addEventListener('click', (e) => {
   const wallCoords = wall.getBoundingClientRect();
 
-  if (e.target === wall) {
-    let coordY = e.clientY - wallCoords.y - wall.clientTop;
-    let coordX = e.clientX - wallCoords.x - wall.clientLeft;
+  let coordY = e.clientY - wallCoords.top - wall.clientTop;
+  let coordX = e.clientX - wallCoords.left - wall.clientLeft;
 
-    if (coordY < spider.clientHeight / 2) {
-      coordY = spider.clientHeight / 2;
-    }
+  coordX = Math.max(
+    spider.clientWidth / 2,
+    Math.min(coordX, wall.clientWidth - spider.clientWidth / 2),
+  );
 
-    if (coordX < spider.clientWidth / 2) {
-      coordX = spider.clientWidth / 2;
-    }
+  coordY = Math.max(
+    spider.clientHeight / 2,
+    Math.min(coordY, wall.clientHeight - spider.clientHeight / 2),
+  );
 
-    if (coordY > wall.clientHeight - spider.clientHeight / 2) {
-      coordY = wall.clientHeight - spider.clientHeight / 2;
-    }
-
-    if (coordX > wall.clientWidth - spider.clientWidth / 2) {
-      coordX = wall.clientWidth - spider.clientWidth / 2;
-    }
-
-    if (
-      coordY > 0 &&
-      coordY < wall.clientHeight &&
-      coordX > 0 &&
-      coordX < wall.clientWidth
-    ) {
-      spider.style.top = coordY - spider.clientHeight / 2 + 'px';
-      spider.style.left = coordX - spider.clientWidth / 2 + 'px';
-    }
-  }
+  spider.style.top = coordY - spider.clientHeight / 2 + 'px';
+  spider.style.left = coordX - spider.clientWidth / 2 + 'px';
 });
